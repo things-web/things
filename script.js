3443a68e9a3fa853a1a7150ca71798836ac160a4
fardeencp <fardeencp7@gmail.com>
2026-01-24 02:35:32 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const starContainer = document.querySelector(".stars");
-
-for (let i = 0; i < 40; i++) {
-  const star = document.createElement("span");
-  star.style.left = Math.random() * 100 + "vw";
-  star.style.animationDuration = (15 + Math.random() * 15) + "s";
-  starContainer.appendChild(star);
-}
